8c392116e2ab26a800b8a7e6e2447166acabca33
plzfgme <plzfgme@mail.ustc.edu.cn>
2022-06-04 16:23:30 +0800

    modify test

--- consprf_test.go
+++ consprf_test.go
@@ -10,14 +10,14 @@ import (
 )
 
 func TestConstrain(t *testing.T) {
-	ggm := consprf.NewGGM(4)
+	ggm := consprf.NewGGM(16)
 	mk := make([]byte, 32)
 	rand.Read(mk)
-	ck := ggm.Constrain(mk, big.NewInt(2), big.NewInt(7))
-	if ggm.EvalCK(ck, big.NewInt(9)) != nil {
+	ck := ggm.Constrain(mk, big.NewInt(4555), big.NewInt(9888))
+	if ggm.EvalCK(ck, big.NewInt(1234)) != nil {
 		t.Error("Evaluation on out-of-range input should returns nil.")
 	}
-	if !bytes.Equal(ggm.EvalCK(ck, big.NewInt(5)), ggm.EvalMK(mk, big.NewInt(5))) {
+	if !bytes.Equal(ggm.EvalCK(ck, big.NewInt(6001)), ggm.EvalMK(mk, big.NewInt(6001))) {
 		t.Error("Evaluation on in-range input should be equal.")
 	}
 }
